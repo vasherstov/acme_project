a1f0eaf94822cc888b854119ad9a1df9c4091d04
Vladimir Sherstov <vasherstov@edu.hse.ru>
2025-11-12 23:48:27 +0300

    New Forms

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from .forms import BirthdayForm
+from .utils import calculate_birthday_countdown
+from .models import Birthday
 
 
-def birthday(request):
-    context = {}
-    return render(request, 'birthday/birthday.html', context=context)
+def birthday(request, pk=None):
+    if pk is not None:
+        instance = get_object_or_404(Birthday, pk=pk)
+    else:
+        instance = None
+    form = BirthdayForm(
+        request.POST or None,
+        files=request.FILES or None,
+        instance=instance
+    )
+    context = {'form': form}
+    if form.is_valid():
+        form.save()
+        birthday_countdown = calculate_birthday_countdown(
+            form.cleaned_data['birthday']
+        )
+        context.update({'birthday_countdown': birthday_countdown})
+    return render(request, 'birthday/birthday.html', context)
+
+def birthday_list(request):
+    birthdays = Birthday.objects.all()
+    context = {'birthdays': birthdays}
+    return render(request, 'birthday/birthday_list.html', context)
+
+def delete_birthday(request, pk):
+    instance = get_object_or_404(Birthday, pk=pk)
+    form = BirthdayForm(instance=instance)
+    context = {'form': form}
+    if request.method == 'POST':
+        instance.delete()
+        return redirect('birthday:list')
+    return render(request, 'birthday/birthday.html', context)
